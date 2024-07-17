99872ba7385f74b50c7bdee002d4e694329482cc
Santosh Bandichode <santoshg550@gmail.com>
2024-07-17 15:34:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].nextElementSibling.style.height = 'auto';
+    acc[i].nextElementSibling.style.padding = '2vh';
+
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (!panel.style.height) {
+            panel.style.height = "auto";
+            panel.style.padding = "2vh";
+        } else {
+            panel.style.height = null;
+            panel.style.padding = null;
+        }
+    });
+}
